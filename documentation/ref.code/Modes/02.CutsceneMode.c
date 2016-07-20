34038d06758206c3900592fd4bd053e1b15d30e6
Zane Wagner <zanewagner@gmail.com>
2016-07-20 18:24:26 -0500

    Update and rename CutsceneMode.c to 02.CutsceneMode.c

---
+++ documentation/ref.code/Modes/02.CutsceneMode.c
@@ -0,0 +1,5 @@
+/*  CutsceneMode displays full-screen text and conversations in the game.
+    It also has a fairly simple scripting system that can set flags, read flags,
+    branch, and display alternate text and conversation options.
+    On end, it returns to WorldMode.
+*/
