667c356b192e6402880493057479c7cdeab61eda
Zane Wagner <zanewagner@gmail.com>
2016-07-20 18:26:17 -0500

    Update and rename WorldMode.c to 03.WorldMode.c

---
+++ documentation/ref.code/Modes/03.WorldMode.c
@@ -0,0 +1,4 @@
+/*  WorldMode is the main game mode, where the player spends most of their time.
+    The player can walk around the world, interact with actors (scripted objects),
+    enter towns, etc.
+*/
