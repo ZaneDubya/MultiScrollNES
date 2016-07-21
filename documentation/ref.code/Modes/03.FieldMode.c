398acdd6edcd418332ec083fc7b77b10dabb1f8c
Zane Wagner <zanewagner@gmail.com>
2016-07-21 13:03:39 -0500

    Update 03.FieldMode.c

--- documentation/ref.code/Modes/03.FieldMode.c
+++ documentation/ref.code/Modes/03.FieldMode.c
@@ -1,4 +1,18 @@
 /*  FieldMode is the main game mode, where the player spends most of their time.
     The player can walk around the world, interact with actors (scripted objects),
     enter towns, etc.
+    States
+    00  Walking around!
+        DPad    Movement
+        A       Do something
+        B       Examine something.
+        Start   Display start menu
+    01  Displaying the START/PAUSE menu at the bottom of the screen:
+        Select character (0, 1, 2), Map, Spells, Rest, Menu
+        Initially, Map is selected.
+        Change selection with Left and Right.
+        Affirm selection with A.
+        Dismiss START menu with START or B.
+    02  Displaying text at the bottom of the screen. 
+        After A or B is pressed, the game will return to the world or enter another mode.
 */
