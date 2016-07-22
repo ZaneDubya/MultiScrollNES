65b093c8f4ad6d7f38692698f6be6887a835fa3a
Zane Wagner <zanewagner@gmail.com>
2016-07-22 09:25:07 -0500

    Update 06.MapMode.c

--- documentation/ref.code/Modes/06.MapMode.c
+++ documentation/ref.code/Modes/06.MapMode.c
@@ -1,3 +1,5 @@
-/* Displays an awesome world map. Great!
-    
+/*  MapMode - Displays an awesome world map.
+    The world is stretched out in a single screen.
+    The player's current location is displayed with an icon.
+    Locations of towns are also displayed.
 */
