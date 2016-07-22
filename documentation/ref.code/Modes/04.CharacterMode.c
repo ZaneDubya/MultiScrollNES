7273cc5114f60f09982a3047692e8b40c9aac195
Zane Wagner <zanewagner@gmail.com>
2016-07-22 09:22:55 -0500

    Update 04.CharacterMode.c

--- documentation/ref.code/Modes/04.CharacterMode.c
+++ documentation/ref.code/Modes/04.CharacterMode.c
@@ -1,9 +1,5 @@
 /*  The player can view information about their characters from this mode.
-    Statistics
-    Inventory
-    ItemDetails
-    Merchant
-    CharBackground
-    SpellBook
-    Haggle
+    00  StatisticsAndInventory
+    01  ItemDetails
+    02  CharBackground
 */
