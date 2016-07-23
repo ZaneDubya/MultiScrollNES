8c49f584f9106c9bff63dcc5163f1128170649cc
Zane Wagner <zanewagner@gmail.com>
2016-07-23 09:59:41 -0500

    Update 05.BattleMode.c

--- documentation/ref.code/Modes/05.BattleMode.c
+++ documentation/ref.code/Modes/05.BattleMode.c
@@ -7,5 +7,45 @@
             bg: 2 palettes for heroes, 2 for enemies, but bottom tile must share a common palette.
             sp: 2 palettes for h/e common palette, 2 pal for selection/special effects.
     Menu - bottom 256x64 (32x8) of the screen.
-        Top, bottom, left, right 8 pixels are blank.
+        Top, bottom, left, right 8 pixels are blank. Furthermore, left and right sides are padded 8px.
+        When an enemy turn is in progress, the menu is blank.
+        When a player turn is in progress, the menu is as follows:
+            Portrait (32x32) (select this to view stats)
+            Name and Stats: Health, Energy, Strength, Armor (96px wide)
+            Actions are 16px wide each, 6, max 96x.
+                ActionMove (and minor attack) (always)
+                ActionAttack (if close to an enemy)
+                ActionSpell (if magic user)
+                ActionSkill (if has skills)
+                ActionItem (always)
+                AttemptEscape (if on edge of map)
+    Modes:
+        00  Loading...
+        01  Get next turn
+        02  End turn
+        03  Begin enemy turn (show menu)
+        04  Enemy turn - do AI
+        05  Begin player turn (calculate options and show menu)
+        06  Waiting for player input - menu
+        07  Calculate player movement range
+        08  Waiting for player input - select move target
+        09  Show spell menu
+        0A  Show skill menu
+        0B  Waiting for player input - select skill/spell
+        0C  Waiting for player input - select skill/spell target
+        0D  Show item menu
+        0E  Waiting for player input - select item
+        0F  Waiting for player input - select item target
+        10  Do item effect on self
+        11  Do item effect on target
+        12  Do actor move
+        13  Do actor attack
+        14  Do actor cast spell
+        15  Do actor use skill
+        16  Do actor use item
+        17  Do actor die
+        18  Do actor get hit
+        19  Do actor buff
+        1A  Do projectile
+        1B  Do player flee
 */
